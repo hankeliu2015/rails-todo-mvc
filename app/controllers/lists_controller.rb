1b2179a4f87e2070d3020e02bb527f10541b59e0
github username <github email address>
2019-12-25 17:02:08 -0500

    srong params in List controller. form_for tag in index page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,26 @@
 class ListsController < ApplicationController
   def index
+    @list = List.new
     @lists = List.all
     # raise @lists.inspect
   end
 
   def show
     @list = List.find_by(id: params[:id])
+  end
+
+  def create
+    # raise params.inspect
+    @list = List.new(list_params)
+    # @list.name = params[:list][:name]
+    @list.save
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
