8e42b31e93cfcfa848b2e96c44291313543a6fb9
github username <github email address>
2019-12-26 23:52:48 -0500

    change status default to 0

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_042126) do
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
   end
 
   create_table "lists", force: :cascade do |t|
