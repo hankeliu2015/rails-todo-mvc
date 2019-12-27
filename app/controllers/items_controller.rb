d79d1925d0372cefd92dcd5464be4b160ce07d03
github username <github email address>
2019-12-26 22:04:57 -0500

    add validation and style for items create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,13 @@ class ItemsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @item = @list.items.build(item_params)
-    @item.save
-    redirect_to list_path(@list)
+
+    if @item.save
+      redirect_to list_path(@list)
     # raise params.inspect
+    else
+      render "lists/show"
+    end
   end
 
   private
