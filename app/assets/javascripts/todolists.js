12f1b22b11e3a18bf68ef8cd67a25d05b06de5a5
github username <github email address>
2020-01-14 20:27:49 -0500

    udpate js file

---
+++ app/assets/javascripts/todolists.js
@@ -0,0 +1,18 @@
+
+$(function(){
+  $("input.toggle").on("change", function(){
+    console.log('worked');
+    $(this).siblings("form").trigger("submit")
+  })
+});
+
+// $(function(){
+//   $("input.toggle").on("change", function(){
+//     console.log('worked');
+//     $(this).parents("form").trigger("submit")
+//   })
+// });
+
+function test123() {
+  console.log("this is the funcion");
+}
